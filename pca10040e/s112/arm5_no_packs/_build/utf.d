e23d379b4bd45d937db18c5e774183b74bbca3e4
yangbo <yangbo@yungengxin.com>
2022-08-19 17:21:01 +0800

    first commit

---
+++ pca10040e/s112/arm5_no_packs/_build/utf.d
@@ -0,0 +1,4 @@
+.\_build\utf.o: ..\..\..\..\..\..\external\utf_converter\utf.c
+.\_build\utf.o: ..\..\..\..\..\..\external\utf_converter\utf.h
+.\_build\utf.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stddef.h
+.\_build\utf.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
